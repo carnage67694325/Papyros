3a687c173ca4072e10ee5f8ef75255cd82a62d68
carnage67694325 <kingabdoeid2014@gmail.com>
2024-10-25 22:44:12 +0300

    generated app icons class

---
+++ lib/core/utils/app_icons.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_single_quotes
+class AppIcons {
+  AppIcons._();
+
+  /// Assets for assetsIconsEmail icon
+  /// assets/icons/Email icon.svg
+  static const String assetsIconsEmailicon = "assets/icons/Email icon.svg";
+
+  /// Assets for assetsIconsLock password icon
+  /// assets/icons/Lock password icon.svg
+  static const String assetsIconsLockPasswordicon =
+      "assets/icons/Lock password icon.svg";
+
+  /// Assets for assetsIconsShow Password icon
+  /// assets/icons/Show Password icon.svg
+  static const String assetsIconsShowPasswordicon =
+      "assets/icons/Show Password icon.svg";
+
+  /// Assets for assetsLogoPapyros
+  /// assets/logo/Papyros.svg
+  static const String assetsLogoPapyros = "assets/logo/Papyros.svg";
+}
