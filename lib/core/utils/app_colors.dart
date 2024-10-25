8ae91330f7a1e2c4420c45b0bdc3dd1647dcf9af
carnage67694325 <kingabdoeid2014@gmail.com>
2024-10-25 22:48:46 +0300

    created app colors class

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryBlue = Color(0xFF6482AD);
+  static const Color lightBlue = Color(0xFF7FA1C3);
+  static const Color lightGreyBlue = Color(0xFFE2DAD6);
+  static const Color darkBrown = Color(0xFF584242);
+  static const Color lightPeach = Color(0xFFE6B9A6);
+  static const Color lightBrown = Color(0xFFC19C8D);
+  static const Color mediumBrown = Color(0xFF896252);
+  static const Color lightYellow = Color(0xFFF6EFBD);
+}
