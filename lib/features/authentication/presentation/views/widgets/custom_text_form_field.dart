3d5d90e7019ad907b29135a7cb00f67ab27e81cf
carnage67694325 <kingabdoeid2014@gmail.com>
2024-11-01 13:23:37 +0200

    added valditor to customtextfromfield

--- lib/features/authentication/presentation/views/widgets/custom_text_form_field.dart
+++ lib/features/authentication/presentation/views/widgets/custom_text_form_field.dart
@@ -2,27 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:papyros/core/utils/app_colors.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({
-    super.key,
-    required this.hintText,
-    this.hintStyle,
-    this.prefixIcon,
-    this.obscureText,
-    this.suffixIcon,
+  const CustomTextFormField(
+      {super.key,
+      required this.hintText,
+      this.hintStyle,
+      this.prefixIcon,
+      this.obscureText,
+      this.suffixIcon,
       this.controller,
-  });
+      this.validator});
   final String hintText;
   final TextStyle? hintStyle;
   final Widget? prefixIcon;
   final bool? obscureText;
   final Widget? suffixIcon;
   final TextEditingController? controller;
+  final String? Function(String?)? validator;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       obscureText: obscureText ?? false,
-      controller:controller ,
+      validator: validator,
+      controller: controller,
       decoration: InputDecoration(
         filled: true,
         fillColor: AppColors.textFieldFillColor.withOpacity(0.7),
@@ -30,7 +32,6 @@ class CustomTextFormField extends StatelessWidget {
         hintStyle: hintStyle,
         prefixIcon: prefixIcon,
         suffixIcon: suffixIcon,
-
         border: const OutlineInputBorder(
           borderSide: BorderSide.none,
           borderRadius: BorderRadius.all(Radius.circular(24)),
