c96a6bd9f879801b9ee8d6b8ce97c10040e91cf2
carnage67694325 <kingabdoeid2014@gmail.com>
2024-11-05 13:03:30 +0200

    created VerfiyOtpRepoImp

--- lib/core/endpoints/endpiont.dart
+++ lib/core/endpoints/endpiont.dart
@@ -1,4 +1,5 @@
 class Endpiont {
   static const String signupEndpiont = ".app/auth/signup";
   static const String signipEndpiont = ".app/auth/signin";
+  static const String verfiyOtpEndpoint = ".app/auth/otp";
 }
