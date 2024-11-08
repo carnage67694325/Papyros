10b5b1955f4851451d11a87efd0b28740a418a20
carnage67694325 <kingabdoeid2014@gmail.com>
2024-11-08 16:51:24 +0200

    created sign up use case

---
+++ lib/features/authentication/sign_up/domain/use_cases/sign_up_usecase.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:papyros/core/errors/failure.dart';
+import 'package:papyros/features/authentication/sign_up/domain/entities/sign_up_entity.dart';
+import 'package:papyros/features/authentication/sign_up/domain/repos/sign_up_repo.dart';
+
+class SignUpUsecase {
+  final SignUpRepo signUpRepo;
+
+  SignUpUsecase({required this.signUpRepo});
+  Future<Either<Failure, SignUpEntity>> call({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+    required String phone,
+    required String gender,
+    required String dob,
+  }) {
+    return signUpRepo.signIn(
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        phone: phone,
+        gender: gender,
+        dob: dob);
+  }
+}
