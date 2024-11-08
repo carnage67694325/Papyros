b1b456522c519ffbe40a07a4d128ea96defa7283
carnage67694325 <kingabdoeid2014@gmail.com>
2024-11-08 16:48:16 +0200

    created sign up repo

---
+++ lib/features/authentication/sign_up/domain/repos/sign_up_repo.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:papyros/core/errors/failure.dart';
+import 'package:papyros/features/authentication/sign_up/domain/entities/sign_up_entity.dart';
+
+abstract class SignUpRepo {
+  Future<Either<Failure, SignUpEntity>> signIn({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+    required String phone,
+    required String gender,
+    required String dob,
+  });
+}
