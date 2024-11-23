ac57705a30d72a0e1a12bfcbb33561e28c3ed6e3
carnage67694325 <kingabdoeid2014@gmail.com>
2024-11-23 12:06:32 +0200

    added new logos

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsIconsCloseSM
+  /// assets/icons/Close_SM.svg
+  static const String assetsIconsCloseSM = "assets/icons/Close_SM.svg";
+
+  /// Assets for assetsIconsEllipse 2
+  /// assets/icons/Ellipse 2.svg
+  static const String assetsIconsEllipse2 = "assets/icons/Ellipse 2.svg";
+
+  /// Assets for assetsIconsEllipse 21
+  /// assets/icons/Ellipse 2-1.svg
+  static const String assetsIconsEllipse21 = "assets/icons/Ellipse 2-1.svg";
+
+  /// Assets for assetsIconsEmail icon
+  /// assets/icons/Email icon.svg
+  static const String assetsIconsEmailIcon = "assets/icons/Email icon.svg";
+
+  /// Assets for assetsIconsLock password icon
+  /// assets/icons/Lock password icon.svg
+  static const String assetsIconsLockPasswordIcon =
+      "assets/icons/Lock password icon.svg";
+
+  /// Assets for assetsIconsShow Password icon
+  /// assets/icons/Show Password icon.svg
+  static const String assetsIconsShowPasswordIcon =
+      "assets/icons/Show Password icon.svg";
+
+  /// Assets for assetsIconsVector
+  /// assets/icons/Vector.svg
+  static const String assetsIconsVector = "assets/icons/Vector.svg";
+
+  /// Assets for assetsLogoPapyros
+  /// assets/logo/Papyros.svg
+  static const String assetsLogoPapyros = "assets/logo/Papyros.svg";
+}
