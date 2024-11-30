ecc994c81026371ee2ff31dcaec69cf40d13cf24
carnage67694325 <kingabdoeid2014@gmail.com>
2024-11-30 17:02:50 +0200

    created pick image method

---
+++ lib/core/utils/functions/pick_image.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:papyros/core/utils/functions/error_snack.dart';
+import 'package:papyros/core/utils/functions/success_snack.dart';
+
+Future<void> pickImage(BuildContext context) async {
+  final ImagePicker picker = ImagePicker();
+
+  try {
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      successSnackBar(context, 'Image selected: ${image.path}');
+    } else {
+      successSnackBar(context, 'No image selected');
+    }
+  } catch (e) {
+    errorSnackBar(context, 'Error picking image: $e');
+  }
+}
