200b6a0c2ab78f92ab1d482896481fda18e38d69
carnage67694325 <kingabdoeid2014@gmail.com>
2025-01-25 13:03:49 +0200

    created chat bot view  body

--- lib/features/chat_bot/presentation/view/chat_bot_view.dart
+++ lib/features/chat_bot/presentation/view/chat_bot_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:papyros/features/chat_bot/presentation/widgets/chat_bot_view_body.dart';
 
 class ChatBotView extends StatelessWidget {
   const ChatBotView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: ChatBotViewBody(),
+    );
   }
 }
---
+++ lib/features/chat_bot/presentation/widgets/chat_bot_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ChatBotViewBody extends StatelessWidget {
+  const ChatBotViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
