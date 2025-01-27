9b0fe1e7d510dfc8b2843e06f6eff9ec10190564
carnage67694325 <kingabdoeid2014@gmail.com>
2025-01-27 17:21:07 +0200

    finished the initial state of chat bot

--- lib/features/chat_bot/presentation/widgets/chat_bot_view_body.dart
+++ lib/features/chat_bot/presentation/widgets/chat_bot_view_body.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:papyros/core/utils/app_colors.dart';
+import 'package:papyros/core/utils/app_styles.dart';
 import 'package:papyros/core/utils/assets.dart';
 import 'package:papyros/core/utils/functions/success_snack.dart';
 import 'package:papyros/features/chat_bot/presentation/widgets/chat_bot_app_bar.dart';
 import 'package:papyros/features/chat_bot/presentation/widgets/chat_bot_logo.dart';
 import 'package:papyros/features/chat_bot/presentation/widgets/send_prompt_textfield.dart';
+import 'package:papyros/generated/l10n.dart';
 
 class ChatBotViewBody extends StatefulWidget {
   const ChatBotViewBody({super.key});
@@ -31,7 +34,38 @@ class _ChatBotViewBodyState extends State<ChatBotViewBody> {
               ),
               const SizedBox(height: 179),
               const ChatBotLogo(),
-              const SizedBox(height: 410),
+              const SizedBox(height: 25),
+              Padding(
+                padding: EdgeInsets.only(left: 22, right: 26),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Align text at the top
+                  children: [
+                    Text(
+                      S.of(context).hello + ",",
+                      style: AppStyles.chatHeader.copyWith(
+                        color: AppColors.lightPeach,
+                        fontSize: 32,
+                      ),
+                    ),
+                    const SizedBox(
+                        width: 8), // Adjust spacing between the two texts
+                    Flexible(
+                      child: Text(
+                        S.of(context).chatBotGreetings,
+                        softWrap: true, // Ensure text wraps if necessary
+                        style: AppStyles.chatHeader.copyWith(
+                          fontSize: 32,
+                        ),
+                        overflow: TextOverflow
+                            .visible, // Allow the text to go beyond if needed
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 290),
               SendPromptTextfield(
                   controller: TextEditingController(),
                   onSend: () {
--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -1,15 +1,4 @@
-// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
-// This is a library that provides messages for a en locale. All the
-// messages from the main program should be duplicated here with the same
-// function name.
-
-// Ignore issues from commonly used lints in this file.
-// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
-// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
-// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
-// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
-
+// GENERATED CODE - DO NOT MODIFY BY HAND
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
 
@@ -62,6 +51,9 @@ class MessageLookup extends MessageLookupByLibrary {
         "signIN": MessageLookupByLibrary.simpleMessage("Sign in"),
         "signUP": MessageLookupByLibrary.simpleMessage("Sign Up"),
         "yourEmail": MessageLookupByLibrary.simpleMessage("Your Email"),
-        "yourPassword": MessageLookupByLibrary.simpleMessage("Your Password")
+        "yourPassword": MessageLookupByLibrary.simpleMessage("Your Password"),
+        "hello": MessageLookupByLibrary.simpleMessage("Hello"),
+        "chatBotGreetings":
+            MessageLookupByLibrary.simpleMessage("How can I help you today?"),
       };
 }
