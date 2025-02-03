9b7524c2b3bcde8fd0e701cdc33a21819af3af09
carnage67694325 <kingabdoeid2014@gmail.com>
2025-02-03 19:20:24 +0200

    refactored chat bot view body and create chat bot greetings state

--- lib/features/chat_bot/presentation/widgets/chat_bot_view_body.dart
+++ lib/features/chat_bot/presentation/widgets/chat_bot_view_body.dart
@@ -6,6 +6,7 @@ import 'package:papyros/core/utils/app_styles.dart';
 import 'package:papyros/core/utils/assets.dart';
 import 'package:papyros/core/utils/functions/success_snack.dart';
 import 'package:papyros/features/chat_bot/presentation/widgets/chat_bot_app_bar.dart';
+import 'package:papyros/features/chat_bot/presentation/widgets/chat_bot_greetings_state.dart';
 import 'package:papyros/features/chat_bot/presentation/widgets/chat_bot_logo.dart';
 import 'package:papyros/features/chat_bot/presentation/widgets/send_prompt_textfield.dart';
 import 'package:papyros/generated/l10n.dart';
@@ -22,55 +23,17 @@ class _ChatBotViewBodyState extends State<ChatBotViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverToBoxAdapter(
           child: Column(
             children: [
-              const SizedBox(height: 25),
-              const Padding(
-                padding: EdgeInsets.only(left: 22, right: 26),
+              SizedBox(height: 25),
+              Padding(
+                padding: EdgeInsets.only(left: 10, right: 26),
                 child: ChatBotAppBar(),
               ),
-              const SizedBox(height: 179),
-              const ChatBotLogo(),
-              const SizedBox(height: 25),
-              Padding(
-                padding: EdgeInsets.only(left: 22, right: 26),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // Align text at the top
-                  children: [
-                    Text(
-                      S.of(context).hello + ",",
-                      style: AppStyles.chatHeader.copyWith(
-                        color: AppColors.lightPeach,
-                        fontSize: 32,
-                      ),
-                    ),
-                    const SizedBox(
-                        width: 8), // Adjust spacing between the two texts
-                    Flexible(
-                      child: Text(
-                        S.of(context).chatBotGreetings,
-                        softWrap: true, // Ensure text wraps if necessary
-                        style: AppStyles.chatHeader.copyWith(
-                          fontSize: 32,
-                        ),
-                        overflow: TextOverflow
-                            .visible, // Allow the text to go beyond if needed
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 290),
-              SendPromptTextfield(
-                  controller: TextEditingController(),
-                  onSend: () {
-                    successSnackBar(context, "Message sent successfully");
-                  })
+              ChatbotGreetingState(),
             ],
           ),
         )
