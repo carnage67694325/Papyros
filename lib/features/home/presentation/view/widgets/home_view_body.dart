0dfdbba5d30a9d9f3da57da511037ecd75f158ff
carnage67694325 <kingabdoeid2014@gmail.com>
2025-03-06 21:48:18 +0200

    modified the ui of post card

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:papyros/core/utils/app_styles.dart';
 import 'package:papyros/features/home/presentation/view/widgets/home_screen_app_bar.dart';
 import 'package:papyros/features/home/presentation/view/widgets/post_card.dart';
-import 'package:papyros/features/home/presentation/view/widgets/user_profile_home_avatar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
-        SliverToBoxAdapter(
+        const SliverToBoxAdapter(
           child: Column(
             spacing: 14,
-            children: [HomeScreenAppBar(), PostCard()],
+            children: [
+              HomeScreenAppBar(),
+            ],
           ),
-        )
+        ),
+        SliverList.builder(
+          itemBuilder: (context, index) => const PostCard(),
+          itemCount: 10,
+        ),
       ],
     );
   }
--- lib/features/home/presentation/view/widgets/post_card.dart
+++ lib/features/home/presentation/view/widgets/post_card.dart
@@ -16,78 +16,88 @@ class PostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 14),
-          child: Row(
-            children: [
-              UserProfileHomeAvatar(
-                userProfileImage:
-                    "https://images.immediate.co.uk/production/volatile/sites/3/2017/12/yoda-the-empire-strikes-back-28a7558.jpg?quality=90&webp=true&resize=800,534",
-                height: 55.h,
-                width: 55.w,
-              ),
-              SizedBox(
-                width: 10.w,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 16.h,
-                  ),
-                  Text(
-                    "Yoda",
-                    style: AppStyles.chatHeader.copyWith(fontSize: 16),
-                  ),
-                  SizedBox(
-                    width: 250.w,
-                    child: Text(
-                      "A different view of the pyramids in Egypt. ",
-                      maxLines: 4,
-                      style: AppStyles.postContent,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    icon: const Icon(
-                        color: AppColors.iconColor, Icons.more_horiz),
-                    onPressed: () {},
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                ],
-              ),
-            ],
-          ),
+    return Container(
+      decoration:
+          const BoxDecoration(color: AppColors.backGroundColor, boxShadow: [
+        BoxShadow(
+          color: Colors.grey,
+          offset: Offset(0, 2),
+          blurRadius: 6,
         ),
-        AspectRatio(
-          aspectRatio: 16 / 9,
-          child: Padding(
-            padding: EdgeInsets.only(
-                left: 65.0.h, right: 32.h, top: 16.h, bottom: 6.h),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: const DecorationImage(
-                  image: CachedNetworkImageProvider(
-                      "https://www.thetimes.com/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F99fb9468-4b44-406d-a4c2-5646791b6367.jpg?crop=1600%2C900%2C0%2C0&resize=1200"),
-                  fit: BoxFit.cover,
+      ]),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14),
+            child: Row(
+              children: [
+                UserProfileHomeAvatar(
+                  userProfileImage:
+                      "https://images.immediate.co.uk/production/volatile/sites/3/2017/12/yoda-the-empire-strikes-back-28a7558.jpg?quality=90&webp=true&resize=800,534",
+                  height: 55.h,
+                  width: 55.w,
+                ),
+                SizedBox(
+                  width: 10.w,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 16.h,
+                    ),
+                    Text(
+                      "Yoda",
+                      style: AppStyles.chatHeader.copyWith(fontSize: 16),
+                    ),
+                    SizedBox(
+                      width: 250.w,
+                      child: Text(
+                        "A different view of the pyramids in Egypt. ",
+                        maxLines: 4,
+                        style: AppStyles.postContent,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      icon: const Icon(
+                          color: AppColors.iconColor, Icons.more_horiz),
+                      onPressed: () {},
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  left: 65.0.h, right: 32.h, top: 16.h, bottom: 6.h),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: const DecorationImage(
+                    image: CachedNetworkImageProvider(
+                        "https://www.thetimes.com/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F99fb9468-4b44-406d-a4c2-5646791b6367.jpg?crop=1600%2C900%2C0%2C0&resize=1200"),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        const PostInteractSection()
-      ],
+          const PostInteractSection()
+        ],
+      ),
     );
   }
 }
