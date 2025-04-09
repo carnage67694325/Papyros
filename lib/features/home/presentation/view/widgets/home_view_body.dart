b066197e2435adf6a64f67dca121a72132b664ef
carnage67694325 <kingabdoeid2014@gmail.com>
2025-04-09 13:33:14 +0200

    add loading animation and refereh while getting posts

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -3,50 +3,138 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:papyros/core/animations/app_loading_animation.dart';
 import 'package:papyros/core/utils/functions/error_snack.dart';
 import 'package:papyros/features/home/presentation/view/manager/get_all_posts/get_all_posts_cubit.dart';
 import 'package:papyros/features/home/presentation/view/widgets/home_screen_app_bar.dart';
 import 'package:papyros/features/home/presentation/view/widgets/post_card.dart';
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
+
+  @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
+  late ScrollController _scrollController;
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    // You can add more scroll-based functionalities here if needed
+    // For example, infinite scrolling by checking if scrolled to bottom
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      // Load more data when reaching the bottom
+      log('Reached the bottom of the list');
+      // Implement pagination logic here if needed
+    }
+  }
+
+  Future<void> _refreshPosts() async {
+    log('Refreshing posts...');
+    await context.read<GetAllPostsCubit>().getAllPosts();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<GetAllPostsCubit, GetAllPostsState>(
-        listener: (context, state) {
-      if (state is GetAllPostsFailure) {
-        errorSnackBar(context, state.errMessage);
-        log(state.errMessage);
-      }
-    }, builder: (context, state) {
-      return CustomScrollView(
-        slivers: [
-          const SliverToBoxAdapter(
-            child: Column(
-              spacing: 14,
-              children: [
-                HomeScreenAppBar(),
-              ],
-            ),
+      listener: (context, state) {
+        if (state is GetAllPostsFailure) {
+          errorSnackBar(context, state.errMessage);
+          log(state.errMessage);
+        }
+      },
+      builder: (context, state) {
+        return RefreshIndicator(
+          key: _refreshIndicatorKey,
+          onRefresh: _refreshPosts,
+          color: Theme.of(context).primaryColor,
+          backgroundColor: Colors.white,
+          strokeWidth: 2.5,
+          displacement: 40,
+          child: CustomScrollView(
+            controller: _scrollController,
+            physics: const AlwaysScrollableScrollPhysics(),
+            slivers: [
+              const SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    HomeScreenAppBar(),
+                    SizedBox(height: 14),
+                  ],
+                ),
+              ),
+              state is GetAllPostsLoading
+                  ? const SliverFillRemaining(
+                      child: Center(
+                        child: AppLoadingAnimation(),
+                      ),
+                    )
+                  : state is GetAllPostsSuccess
+                      ? SliverList.builder(
+                          itemBuilder: (context, index) => PostCard(
+                            description: state.posts[index].description!,
+                            userName: state.posts[index].createdBy!.userName ??
+                                "user-name",
+                            userProfileImageUrl: state
+                                    .posts[index].createdBy!.profileImage ??
+                                "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/man-user-circle-icon.png",
+                            imageUrl: state.posts[index].images
+                                ?.map((e) => e.image)
+                                .toList(),
+                          ),
+                          itemCount: state.posts.length,
+                        )
+                      : SliverToBoxAdapter(
+                          child: state is GetAllPostsFailure
+                              ? const Center(
+                                  child: Padding(
+                                    padding: EdgeInsets.all(20.0),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(Icons.error_outline,
+                                            size: 48, color: Colors.grey),
+                                        SizedBox(height: 16),
+                                        Text(
+                                          'Failed to load posts',
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text(
+                                          'Pull down to try again',
+                                          style: TextStyle(
+                                              fontSize: 14, color: Colors.grey),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              : const SizedBox(),
+                        ),
+            ],
           ),
-          state is GetAllPostsSuccess
-              ? SliverList.builder(
-                  itemBuilder: (context, index) => PostCard(
-                    description: state.posts[index].description!,
-                    userName:
-                        state.posts[index].createdBy!.userName ?? "user-name",
-                    userProfileImageUrl: state
-                            .posts[index].createdBy!.profileImage ??
-                        " https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/man-user-circle-icon.png",
-                    imageUrl:
-                        state.posts[index].images?.map((e) => e.image).toList(),
-                  ),
-                  itemCount: state.posts.length,
-                )
-              : const SliverToBoxAdapter(child: SizedBox()),
-        ],
-      );
-    });
+        );
+      },
+    );
   }
 }
