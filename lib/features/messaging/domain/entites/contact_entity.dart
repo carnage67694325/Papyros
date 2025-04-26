1fbac069cfee858f9e3297a1edb56c6cecef6e6d
carnage67694325 <kingabdoeid2014@gmail.com>
2025-04-26 14:24:34 +0300

    created contact entity

---
+++ lib/features/messaging/domain/entites/contact_entity.dart
@@ -0,0 +1,11 @@
+class ContactEntity {
+  final String? id;
+  final String? name;
+  final String? email;
+
+  ContactEntity({
+    required this.id,
+    required this.name,
+    required this.email,
+  });
+}
